74ec8afb2784d720cce1e0ce3571dc1bb7f46aaf
Hongssi96 <paul960801@gmail.com>
2025-11-28 10:29:49 +0900

    feature : User Entity 작성, Gender Enum 작성

---
+++ user-service/src/main/java/com/bidulgi/userservice/domain/model/User.java
@@ -0,0 +1,53 @@
+package com.bidulgi.userservice.domain.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.bidulgi.common.model.BaseEntity;
+import com.bidulgi.common.model.Role;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_user")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder(access = AccessLevel.PROTECTED)
+public class User extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	@Column(name = "id", nullable = false, updatable = false)
+	private UUID id;
+
+	@Column(nullable = false, length = 50)
+	private String name;
+
+	private String nickname;
+
+	@Column(nullable = false, unique = true, length = 100)
+	private String email;
+
+	private String password;
+
+	private LocalDate birthday;
+
+	@Enumerated(EnumType.STRING)
+	private Gender gender;
+
+	@Enumerated(EnumType.STRING)
+	private Role role;
+}
