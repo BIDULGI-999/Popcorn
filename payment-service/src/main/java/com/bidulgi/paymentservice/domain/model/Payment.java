1f2e7bfd6cdd172b0aeae7e7f63aa604258dce80
mathos6147 <kgw6147@naver.com>
2025-12-03 22:12:46 +0900

    feat(payment): Entity 생성

---
+++ payment-service/src/main/java/com/bidulgi/paymentservice/domain/model/Payment.java
@@ -0,0 +1,74 @@
+package com.bidulgi.paymentservice.domain.model;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "p_payment")
+public class Payment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
+
+	@Column(nullable = false)
+	private String paymentKey;
+
+	@Column(name = "user_id", nullable = false)
+	private UUID userId;
+
+	@Column(nullable = false)
+	private String orderId;
+
+	private String orderName;
+
+	@Column(nullable = false)
+	private Integer price;
+
+	private String method;
+
+	private Integer balanceAmount;
+
+	private boolean isPartialCancelable = false;
+
+	@Enumerated(EnumType.STRING)
+	private PaymentStatus status;
+
+	private LocalDateTime approvedAt;
+
+	private LocalDateTime canceledAt;
+
+	@Builder
+	public Payment(UUID userId, String paymentKey, Integer price, String orderId) {
+		this.id = UUID.randomUUID();
+		this.userId = userId;
+		this.paymentKey = paymentKey;
+		this.price = price;
+		this.orderId = orderId;
+		this.status = PaymentStatus.READY;
+	}
+
+	public void approve(String method, LocalDateTime approvedAt, boolean isPartialCancelable) {
+		this.status = PaymentStatus.DONE;
+		this.approvedAt = approvedAt;
+		this.isPartialCancelable = isPartialCancelable;
+		this.method = method;
+	}
+
+	// TODO: 결제 취소, 부분 취소
+}
