32b1108b61dca3116c044a33572fb38c71ae1e35
JIUNG YANG <yangjiung286@gmail.com>
2025-12-04 20:04:08 +0900

    feat(queue): 작업 큐 할당 및 해제 기능 구현

--- queue-service/src/main/java/com/bidulgi/queueservice/application/QueueService.java
+++ queue-service/src/main/java/com/bidulgi/queueservice/application/QueueService.java
@@ -1,12 +1,12 @@
 package com.bidulgi.queueservice.application;
 
-import java.util.UUID;
+import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.bidulgi.queueservice.application.dto.QueueResult;
 import com.bidulgi.queueservice.application.port.TokenGenerator;
-import com.bidulgi.queueservice.domain.model.QueueState;
+import com.bidulgi.queueservice.domain.vo.QueueState;
 import com.bidulgi.queueservice.domain.repository.QueueRepository;
 import com.bidulgi.queueservice.domain.repository.TokenRepository;
 
@@ -25,16 +25,39 @@ public class QueueService {
 		return queueRepository.enqueue(userId, productId)
 			.flatMap(state -> {
 				if (state == QueueState.WAIT) {
-					return Mono.just(QueueResult.of(productId, state));
+					return Mono.just(QueueResult.of(userId, productId, state));
 				}
 
 				return tokenRepository.findToken(userId, productId)
-					.map(token -> QueueResult.of(productId, state, token))
+					.map(token -> QueueResult.of(userId, productId, state, token))
 					.switchIfEmpty(tokenGenerator.createAccessToken(userId, productId)
 						.flatMap(token -> tokenRepository.saveToken(userId, productId, token)
-							.thenReturn(QueueResult.of(productId, state, token)))
+							.thenReturn(QueueResult.of(userId, productId, state, token)))
 					);
 			});
+	}
+
+	// TODO 예약완료 이벤트 수신 시 dequeue 로직 실행하도록 변경
+	public Mono<QueueResult> dequeue(String userId, String productId) {
+		return queueRepository.dequeue(userId, productId)
+			.flatMap(result -> {
+				List<String> results = parseDequeueResult(result);
+				String nextProductId = results.get(0);
+				String nextUserId = results.get(1);
+
+				return tokenGenerator.createAccessToken(nextUserId, nextProductId)
+					.flatMap(token -> tokenRepository.saveToken(nextUserId, nextProductId, token)
+						.thenReturn(QueueResult.of(nextUserId, nextProductId, QueueState.ACTIVE, token)));
+			});
+	}
+
+	private List<String> parseDequeueResult(String result) {
+		String[] parts = result.split(":");
+		if (parts.length != 2) {
+			// TODO 커스텀 예외 처리
+			throw new IllegalArgumentException("Invalid dequeue result format" + result);
+		}
+		return List.of(parts[0], parts[1]);
 	}
 
 }
--- queue-service/src/main/java/com/bidulgi/queueservice/domain/repository/QueueRepository.java
+++ queue-service/src/main/java/com/bidulgi/queueservice/domain/repository/QueueRepository.java
@@ -1,11 +1,13 @@
 package com.bidulgi.queueservice.domain.repository;
 
-import com.bidulgi.queueservice.domain.model.QueueState;
+import com.bidulgi.queueservice.domain.vo.QueueState;
 
 import reactor.core.publisher.Mono;
 
 public interface QueueRepository {
 
 	Mono<QueueState> enqueue(String userId, String productId);
+
+	Mono<String> dequeue(String userId, String productId);
 
 }
