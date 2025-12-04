ba0e8683151b4af34e790405c4d129a672544b7e
JIUNG YANG <yangjiung286@gmail.com>
2025-12-04 19:58:53 +0900

    feat(queue): QueueResult 유저 아이디 필드 추가

--- queue-service/src/main/java/com/bidulgi/queueservice/application/dto/QueueResult.java
+++ queue-service/src/main/java/com/bidulgi/queueservice/application/dto/QueueResult.java
@@ -1,19 +1,20 @@
 package com.bidulgi.queueservice.application.dto;
 
-import com.bidulgi.queueservice.domain.model.QueueState;
+import com.bidulgi.queueservice.domain.vo.QueueState;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public record QueueResult(
+	String userId,
 	String productId,
 	String state,
 	String token
 ) {
-	public static QueueResult of(String productId, QueueState state, String token) {
-		return new QueueResult(productId, state.name(), token);
+	public static QueueResult of(String userId, String productId, QueueState state, String token) {
+		return new QueueResult(userId, productId, state.name(), token);
 	}
 
-	public static QueueResult of(String productId, QueueState state) {
-		return new QueueResult(productId, state.name(), null);
+	public static QueueResult of(String userId, String productId, QueueState state) {
+		return new QueueResult(userId, productId, state.name(), null);
 	}
 }
