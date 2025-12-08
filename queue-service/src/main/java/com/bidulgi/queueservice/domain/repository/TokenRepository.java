2077d9b08d6bb2edffa6ee90421d302b08ff3d34
JIUNG YANG <yangjiung286@gmail.com>
2025-12-08 20:21:50 +0900

    feat(queue): 대기 순번 조회(SSE) 기능 구현

--- queue-service/src/main/java/com/bidulgi/queueservice/domain/repository/TokenRepository.java
+++ queue-service/src/main/java/com/bidulgi/queueservice/domain/repository/TokenRepository.java
@@ -4,7 +4,20 @@ import reactor.core.publisher.Mono;
 
 public interface TokenRepository {
 
+	/**
+	 * 토큰 조회
+	 * @param userId 유저 아이디
+	 * @param productId 상품 아이디
+	 * @return 조회된 토큰
+	 */
 	Mono<String> findToken(String userId, String productId);
 
+	/**
+	 * 토큰 저장
+	 * @param userId 유저 아이디
+	 * @param productId 상품 아이디
+	 * @param token 토큰
+	 * @return 저장 성공 여부
+	 */
 	Mono<Boolean> saveToken(String userId, String productId, String token);
 }
