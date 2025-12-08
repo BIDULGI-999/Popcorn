93a47fa0aae7b67ca15caed7efd8b6bfa651b3e5
mathos6147 <kgw6147@naver.com>
2025-12-08 16:15:51 +0900

    feature(payment): Jpa repository 수정

--- payment-service/src/main/java/com/bidulgi/paymentservice/infrastructure/repository/JpaPaymentRepository.java
+++ payment-service/src/main/java/com/bidulgi/paymentservice/infrastructure/repository/JpaPaymentRepository.java
@@ -1,5 +1,6 @@
 package com.bidulgi.paymentservice.infrastructure.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,4 +8,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bidulgi.paymentservice.domain.model.Payment;
 
 public interface JpaPaymentRepository extends JpaRepository<Payment, UUID> {
+
+	Optional<Payment> findByOrderId(String orderId);
 }
