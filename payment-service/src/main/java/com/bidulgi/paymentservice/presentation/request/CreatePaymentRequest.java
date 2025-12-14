5b720bf29ba8013a2ce4053fe13c1dd72e54d5cc
mathos6147 <kgw6147@naver.com>
2025-12-14 22:35:04 +0900

    feature(payment): 결제 생성 dto 수정

--- payment-service/src/main/java/com/bidulgi/paymentservice/presentation/request/CreatePaymentRequest.java
+++ payment-service/src/main/java/com/bidulgi/paymentservice/presentation/request/CreatePaymentRequest.java
@@ -1,10 +1,13 @@
 package com.bidulgi.paymentservice.presentation.request;
 
-import com.bidulgi.paymentservice.domain.model.Payment;
+import com.bidulgi.paymentservice.application.dto.ConfirmPaymentCommand;
 
 public record CreatePaymentRequest(
 	String orderId,
 	Integer amount,
 	String paymentKey
 ) {
+	public ConfirmPaymentCommand toCommand(){
+		return new ConfirmPaymentCommand(orderId, paymentKey, amount);
+	}
 }
